ee200e4d67f7dca162dccd4d0634907c30d4be2c
Lars <lars.wolfram@devlab.expert>
2024-01-07 14:18:06 +0100

    add support for linux subsystem

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
-const path = require('path');
-const fs = require('fs');
 const fspromise =  require('fs/promises');
 const os = require('os');
+function parseArguments(input) {
+    const cdArgument = input.replace(/cd\s/g, '').trim();
+    const splittedArguments = cdArgument.split('/');
+    const search =  splittedArguments[splittedArguments.length - 1].trim();
+    const dirs =  splittedArguments.length > 1 ? splittedArguments.splice(0, splittedArguments.length - 1) : [];
+    return {search, dirs};
+}
+
+function isLinuxSubsystem(dirs) {
+    return os.platform() === 'win32' && dirs[0] == 'mnt';
+}
+
+function constructSearchDir(dirsA, dirsB) {
+    const d = isLinuxSubsystem(dirsA) ? dirsA.slice(1) : dirsA;
+    const directories = [...d,  ...dirsB];
+    if(os.platform() === 'win32') {
+        const drive = directories[0];
+        const path = directories.reduce((a, v, currentIndex) => (currentIndex > 1 ? a : '') + (currentIndex > 1 ? '\\' : '') + v.replace(' ', '` '), '');
+        return `${drive}:\\${path}`;
+    } else {
+        return directories.reduce((a, v, currentIndex) => a + '/' + v.replace(' ', '\\ '), '');
+    }
+}
+
+function constructCommand(dirs, directoryName, pathStyleWindows) {
+    const directories = [...dirs,  directoryName];
+    if(pathStyleWindows) {
+        return `cd ${directories.reduce((a, v, currentIndex) =>  a + (currentIndex > 0 ? '\\' : '') + v.replace(' ', '` '), '')}\\`;
+    } else {
+        return `cd ${directories.reduce((a, v, currentIndex) =>  a + (currentIndex > 0 ? '/' : '') + v.replace(' ', '\\ '), '')}/`;
+    }
+}
+
 async function suggest(data) {
-  if(os.platform() === 'win32') {
-    data.directory[0] = data.directory[0]+':'
-  } else {
-    data.directory.unshift('/');
-  }
-  const cdArgument = data.currentInput.input.replace(/cd\s/g, '').trim();
-  const cdDir = cdArgument.split('/');
-  const partFromCd = cdDir.length > 1 ? cdDir.splice(0, cdDir.length - 1) : [];
-  console.log('###########', partFromCd);
-  const currentDir = path.join(...data.directory, ...partFromCd);
-  const partOfDirName = cdDir[cdDir.length - 1];
-  return (await fspromise.readdir(currentDir, { withFileTypes: true }))
-    .filter(file => file.isDirectory() && file.name.startsWith(partOfDirName))
-    .map(directory => {
-      const label = directory.name;
-      const command = `cd ${partFromCd.length === 0 ? '' : path.join(...partFromCd) + '/'}${directory.name}/`;
-      return {label, command};
-    });
+    const argument = parseArguments(data.currentInput.input);
+    const searchDir = constructSearchDir(data.directory, argument.dirs);
+    return (await fspromise.readdir(searchDir, { withFileTypes: true }))
+        .filter(file => file.isDirectory() && file.name.toLowerCase().startsWith(argument.search.toLowerCase()))
+        .map(directory => {
+            const label = directory.name;
+            const command = constructCommand(argument.dirs, directory.name, data.location.shellType.toLowerCase().startsWith('powershell'));
+            return {label, command};
+        });
 }
 
 module.exports = {suggest};
